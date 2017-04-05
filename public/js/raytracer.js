202de2b2849263dd36fa491cc79ebf855a0b0ecd
Samuel Carlsson <samuel.carlsson@gmail.com>
2017-04-05 20:58:15 +0200

    draw pixels

---
+++ public/js/raytracer.js
@@ -0,0 +1,18 @@
+function onload() {
+    draw();
+}
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  
+  var img = ctx.createImageData(320, 200);
+  for (var i = 0; i < img.width * img.height * 4; i += 4) {
+      img.data[i + 0] = 0xff;
+      img.data[i + 1] = 128;
+      img.data[i + 2] = 128;
+      img.data[i + 3] = 0xff;
+  }
+
+  ctx.putImageData(img, 0, 0);
+}
